3f2dd3e2a8f62913f5b4163926420933675d04a3
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-07-30 04:50:19 +0530

    ʜᴀʀʀʏ

---
+++ ShrutiMusic/plugins/tools/goodnight.py
@@ -0,0 +1,43 @@
+import random
+from pyrogram import filters
+from pyrogram.types import Message
+from ShrutiMusic import app
+
+
+def get_random_sticker():
+    stickers = [
+        "CAACAgQAAx0Ce9_hCAACaEVlwn7HeZhgwyVfKHc3WUGC_447IAACLgwAAkQwKVPtub8VAR018x4E",
+        "CAACAgIAAx0Ce9_hCAACaEplwn7dvj7G0-a1v3wlbN281RMX2QACUgwAAligOUoi7DhLVTsNsh4E",
+        "CAACAgIAAx0Ce9_hCAACaFBlwn8AAZNB9mOUvz5oAyM7CT-5pjAAAtEKAALa7NhLvbTGyDLbe1IeBA",
+        "CAACAgUAAx0CcmOuMwACldVlwn9ZHHF2-S-CuMSYabwwtVGC3AACOAkAAoqR2VYDjyK6OOr_Px4E",
+        "CAACAgIAAx0Ce9_hCAACaFVlwn-fG58GKoEmmZpVovxEj4PodAACfwwAAqozQUrt2xSTf5Ac4h4E",
+    ]
+    return random.choice(stickers)
+
+
+def get_random_emoji():
+    return random.choice(["😴", "😪", "💤"])
+
+
+@app.on_message(
+    filters.command(
+        ["gn", "n", "oodnight", "ood Night", "ood night"],
+        prefixes=["/", "g", "G"],
+    )
+)
+async def goodnight_command_handler(_, message: Message):
+    sender = message.from_user.mention
+    if random.choice([True, False]):
+        await app.send_sticker(message.chat.id, get_random_sticker())
+        await message.reply_text(
+            f"❖ ɢᴏᴏᴅ ɴɪɢʜᴛ ❖ sᴡᴇᴇᴛ ᴅʀᴇᴀᴍs ❖\n\n"
+            f"❍  {sender} 😴 \n\n"
+            f"❖ ɢᴏ ᴛᴏ ➥ sʟᴇᴇᴘ ᴇᴀʀʟʏ"
+        )
+    else:
+        emoji = get_random_emoji()
+        await message.reply_text(
+            f"❖ ɢᴏᴏᴅ ɴɪɢʜᴛ ❖ sᴡᴇᴇᴛ ᴅʀᴇᴀᴍs ❖\n\n"
+            f"❍  {sender} {emoji} \n\n"
+            f"❖ ɢᴏ ᴛᴏ ➥ sʟᴇᴇᴘ ᴇᴀʀʟʏ"
+        )
