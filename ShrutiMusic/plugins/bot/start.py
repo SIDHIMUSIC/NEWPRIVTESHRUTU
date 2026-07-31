ef2d6ca6518e41fc7b3c8f207c3be268442cbe52
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-07-31 11:59:15 +0530

    ʜᴀʀʀʏ

--- ShrutiMusic/plugins/bot/start.py
+++ ShrutiMusic/plugins/bot/start.py
@@ -1,7 +1,7 @@
 import time
 
 from pyrogram import filters
-from pyrogram.enums import ChatType
+from pyrogram.enums import ChatType, ParseMode
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from py_yt import VideosSearch
 import config
@@ -24,7 +24,7 @@ from config import BANNED_USERS
 from strings import get_string
 
 
-@app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
+@app.on_message(filters.command(["start"]) & filters.private & \~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
@@ -38,12 +38,14 @@ async def start_pm(client, message: Message, _):
                     caption=_["help_1"].format(config.SUPPORT_GROUP),
                     reply_markup=keyboard,
                     message_effect_id=5159385139981059251,
+                    parse_mode=ParseMode.HTML,
                 )
             except:
                 return await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["help_1"].format(config.SUPPORT_GROUP),
                     reply_markup=keyboard,
+                    parse_mode=ParseMode.HTML,
                 )
         if name[0:3] == "sud":
             await sudoers_list(client=client, message=message, _=_)
@@ -51,6 +53,7 @@ async def start_pm(client, message: Message, _):
                 return await app.send_message(
                     chat_id=config.LOG_GROUP_ID,
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                    parse_mode=ParseMode.HTML,
                 )
             return
         if name[0:3] == "inf":
@@ -86,6 +89,7 @@ async def start_pm(client, message: Message, _):
                     caption=searched_text,
                     reply_markup=key,
                     message_effect_id=5159385139981059251,
+                    parse_mode=ParseMode.HTML,
                 )
             except:
                 await app.send_photo(
@@ -93,11 +97,13 @@ async def start_pm(client, message: Message, _):
                     photo=thumbnail,
                     caption=searched_text,
                     reply_markup=key,
+                    parse_mode=ParseMode.HTML,
                 )
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOG_GROUP_ID,
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                    parse_mode=ParseMode.HTML,
                 )
         if name == "start":
             out = private_panel(_)
@@ -108,17 +114,20 @@ async def start_pm(client, message: Message, _):
                     caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM),
                     reply_markup=InlineKeyboardMarkup(out),
                     message_effect_id=5159385139981059251,
+                    parse_mode=ParseMode.HTML,
                 )
             except:
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM),
                     reply_markup=InlineKeyboardMarkup(out),
+                    parse_mode=ParseMode.HTML,
                 )
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOG_GROUP_ID,
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                    parse_mode=ParseMode.HTML,
                 )
     else:
         out = private_panel(_)
@@ -129,21 +138,24 @@ async def start_pm(client, message: Message, _):
                 caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM),
                 reply_markup=InlineKeyboardMarkup(out),
                 message_effect_id=5159385139981059251,
+                parse_mode=ParseMode.HTML,
             )
         except:
             await message.reply_photo(
                 photo=config.START_IMG_URL,
                 caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM),
                 reply_markup=InlineKeyboardMarkup(out),
+                parse_mode=ParseMode.HTML,
             )
         if await is_on_off(2):
             return await app.send_message(
                 chat_id=config.LOG_GROUP_ID,
                 text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                parse_mode=ParseMode.HTML,
             )
 
 
-@app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["start"]) & filters.group & \~BANNED_USERS)
 @LanguageStart
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
@@ -154,12 +166,14 @@ async def start_gp(client, message: Message, _):
             caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
             reply_markup=InlineKeyboardMarkup(out),
             message_effect_id=5159385139981059251,
+            parse_mode=ParseMode.HTML,
         )
     except:
         await message.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
             reply_markup=InlineKeyboardMarkup(out),
+            parse_mode=ParseMode.HTML,
         )
     return await add_served_chat(message.chat.id)
 
@@ -187,6 +201,7 @@ async def welcome(client, message: Message):
                             config.SUPPORT_GROUP,
                         ),
                         disable_web_page_preview=True,
+                        parse_mode=ParseMode.HTML,
                     )
                     return await app.leave_chat(message.chat.id)
 
@@ -202,6 +217,7 @@ async def welcome(client, message: Message):
                         ),
                         reply_markup=InlineKeyboardMarkup(out),
                         message_effect_id=5159385139981059251,
+                        parse_mode=ParseMode.HTML,
                     )
                 except:
                     await message.reply_photo(
@@ -213,6 +229,7 @@ async def welcome(client, message: Message):
                             app.mention,
                         ),
                         reply_markup=InlineKeyboardMarkup(out),
+                        parse_mode=ParseMode.HTML,
                     )
                 await add_served_chat(message.chat.id)
                 await message.stop_propagation()
