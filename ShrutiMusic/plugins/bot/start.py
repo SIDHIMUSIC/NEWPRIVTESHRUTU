81fecb623a62b75a2ffe17402c450577e58914c4
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-08-03 04:27:54 +0530

    ʜᴀʀʀʏ

--- ShrutiMusic/plugins/bot/start.py
+++ ShrutiMusic/plugins/bot/start.py
@@ -43,7 +43,7 @@ def start_text(user_name: str) -> str:
     )
 
 
-@app.on_message(filters.command(["start"]) & filters.private & \~BANNED_USERS)
+@app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
@@ -135,7 +135,7 @@ async def start_pm(client, message: Message, _):
             )
 
 
-@app.on_message(filters.command(["start"]) & filters.group & \~BANNED_USERS)
+@app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
